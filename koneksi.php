4b1c55c601870d0c2144410851eca5c090c3d011
SMK YAPPI Wonosari <103823142+smkyappiwns@users.noreply.github.com>
2026-02-04 19:24:01 +0700

    Update database connection parameters in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,8 @@
 <?php
 $host     = 'localhost';
-$user     = 'root'; // diisi dengan user database kalian biasanya
-                    // defaultnya bernama root jika kita belum 
-                    // merubahnya
-$password = '';  //diisi dengan password database kalian biasanya
-                 // defaultnya kosong
-$db       = 'belajar_authentikasi'; //diisi dengan nama database kalian
+$user     = 'root';
+$password = ''; 
+$db       = 'login';
  
 $con = mysqli_connect($host, $user, $password, $db) or die(mysqli_error());
 ?>
